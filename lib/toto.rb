bda7a9eab593e7a577d319ddeb32da7899f9dab3
Elise Huard <elise@elisehuard.be>
2010-09-08 21:40:12 +0200

    making archives available to articles

--- lib/toto.rb
+++ lib/toto.rb
@@ -90,7 +90,7 @@ module Toto
     end
 
     def article route
-      Article.new("#{Paths[:articles]}/#{route.join('-')}.#{self[:ext]}", @config).load
+      {:article => Article.new("#{Paths[:articles]}/#{route.join('-')}.#{self[:ext]}", @config).load }.merge archives
     end
 
     def /
